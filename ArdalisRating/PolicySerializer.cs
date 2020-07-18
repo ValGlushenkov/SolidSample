b60b2db28d5749430f4831e54d3e417f74afb02e
VAL <VAL@LAPTOP-4U4U2161>
2020-07-17 19:21:47 -0600

    Added changes recommended by instructor.

---
+++ ArdalisRating/PolicySerializer.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ArdalisRating
+{
+    public class PolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string json) 
+        {
+            return JsonConvert.DeserializeObject<Policy>(json, new StringEnumConverter());
+        }
+    }
+}
