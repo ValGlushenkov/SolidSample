4234b0065915e815bbc68cba0ea4b0ba30054e29
VAL <VAL@LAPTOP-4U4U2161>
2020-07-23 17:59:51 -0600

    Added a null check

--- ArdalisRating/RaterFactory.cs
+++ ArdalisRating/RaterFactory.cs
@@ -18,6 +18,7 @@ namespace ArdalisRating
             catch
             {
                 return null;
+                //return new UnknownPolicyRater(engine, engine.Logger);
             }
         }
     }
