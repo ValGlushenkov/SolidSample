6c52b155cf4b6c14668a21598bf9ed2eb31cd824
VAL <VAL@LAPTOP-4U4U2161>
2020-08-05 20:33:40 -0600

    Added a web api project

---
+++ ArdalisRating/Infrastructure/PolicySources/StringPolicySource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class StringPolicySource : IPolicySource
+    {
+        public string PolicyString { private get; set; }
+        private string _policyString = string.Empty;
+        public string GetPolicyFromSource()
+        {
+            _policyString = PolicyString;
+            return _policyString;
+        }
+    }
+}
---
+++ WebRating/Controllers/RateController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ArdalisRating;
+
+namespace WebRating.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class RateController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        private readonly RatingEngine _ratingEngine;
+        private readonly StringPolicySource _policySource;
+
+        public RateController(RatingEngine ratingEngine,
+            StringPolicySource policySource)
+        {
+            _ratingEngine = ratingEngine;
+            _policySource = policySource;
+        }
+
+        [HttpPost]
+        public ActionResult<decimal> Rate([FromBody] string policy)
+        {
+            _policySource.PolicyString = policy;
+            _ratingEngine.Rate();
+
+            return _ratingEngine.Rating;
+        }
+    }
+}
